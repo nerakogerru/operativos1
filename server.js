8694851e52f3f376c36dacf54bc2343113a540db
root <karen.urrego@unillanos.edu.co>
2015-04-18 18:03:07 -0500

    iojs

---
+++ server.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require ('socket.io')(http);
+
+io.on('connection', function(socket){
+	console.log('usuario conectado');
+});
+http.listen(3000, function(){
+	console.log('listening on htpp://127.0.0.1:3000');
+});
